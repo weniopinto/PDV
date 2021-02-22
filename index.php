18a1a9e7b12e4077def59f0fabbabe3e922f5c78
weniopinto <wenio.pinto@gdppi.ifce.edu.br>
2021-02-22 14:09:32 -0300

    inicio

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+      <link type="text/css" rel="stylesheet" href="css/css.css">
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+     
+      
+    </head>
+
+      <!--NAVBAR-->
+      <nav>
+        <div class="nav-wrapper purple">
+          <a href="#!" class="brand-logo center" style="font-family: arial;">CONTROLE DE ESTOQUE</a>
+          <ul class="right hide-on-med-and-down">
+            <li><a href="#">Produtos<i class="material-icons left">add_box</i></a></li>
+            <li><a href="#">Venda<i class="material-icons left">local_atm</i></a></li>
+            <li><a href="#">Relatórios<i class="material-icons left">insert_chart</i></a></li>
+          </ul>
+          <ul class="left hide-on-med-and-down">
+            <li class="active"><a href="#">Início<i class="material-icons left">home</i></a></li>
+          </ul>
+        </div>
+      </nav>   
+      <!--NAVBAR-->  
+
+    <body>
+        
+    
+      <!--JavaScript-->
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+      <script type="text/javascript" src="js/materialize.js"></script>
+    </body>
+
+  </html>
+        
